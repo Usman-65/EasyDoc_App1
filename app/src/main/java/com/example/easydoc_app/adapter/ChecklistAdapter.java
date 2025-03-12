72175b068ff649da7559ec4a286ea718328f0a76
MKdev203 <magnus.kietzig@gmail.com>
2025-03-12 17:16:53 +0100

    Checklisten Inhalt updaten funktionsfähig gemacht

--- app/src/main/java/com/example/easydoc_app/adapter/ChecklistAdapter.java
+++ app/src/main/java/com/example/easydoc_app/adapter/ChecklistAdapter.java
@@ -6,6 +6,8 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.text.Editable;
+import android.text.TextWatcher;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import com.example.easydoc_app.R;
@@ -34,12 +36,24 @@ public class ChecklistAdapter extends RecyclerView.Adapter<ChecklistAdapter.View
         String item = checklistItems.get(position);
         holder.editChecklistItem.setText(item);
 
-        // Speichern der Änderungen, wenn der Benutzer den Text ändert
-        holder.editChecklistItem.setOnFocusChangeListener((v, hasFocus) -> {
-            if (!hasFocus) { // Speichern, wenn der Nutzer das Feld verlässt
-                checklistItems.set(position, holder.editChecklistItem.getText().toString());
+        holder.editChecklistItem.removeTextChangedListener(holder.textWatcher);
+
+        holder.textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Während getippt wird, gleich den neuen Wert speichern
+                checklistItems.set(holder.getAdapterPosition(), s.toString());
             }
-        });
+
+            @Override
+            public void afterTextChanged(Editable s) { }
+        };
+
+        // Listener setzen
+        holder.editChecklistItem.addTextChangedListener(holder.textWatcher);
 
         holder.deleteButton.setOnClickListener(v -> {
             checklistItems.remove(position);
@@ -56,6 +70,7 @@ public class ChecklistAdapter extends RecyclerView.Adapter<ChecklistAdapter.View
         CheckBox checkBox;
         EditText editChecklistItem;
         Button deleteButton;
+        TextWatcher textWatcher;
 
         ViewHolder(View itemView) {
             super(itemView);
